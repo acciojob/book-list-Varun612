7caddf3e2e3fe807376273c4fab1054eec77663d
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-14 20:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const title=document.getElementById('title');
+const author=document.getElementById('author');
+const isbn=document.getElementById('isbn');
+const add=document.getElementById('submit');
+const bodyRow=document.getElementById('book-list');
+add.addEventListener('click',()=> {
+	const titleInput=title.value;
+	const authorInput=author.value;
+	const isbnInput=isbn.value;
+	const  newRow = document.createElement('tr');
+	newRow.innerHTML = `
+	<td>${titleInput}</td>
+	<td>${authorInput}</td>
+	<td>${isbnInput}</td>
+	<td><button class="delete">Delete</button></td>
+	`;
+	bodyRow.appendChild(newRow);
+	title.value='';
+	author.value='';
+	isbn.value='';
+	newRow.querySelector('.delete').addEventListener('click', () => {
+		newRow.remove();
+	});
+});
